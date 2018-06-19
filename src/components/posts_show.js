9815db10f020977fd3fe9c73915dd63f8715258b
Natthaphon Phanit <nulladivides@protonmail.com>
2018-06-19 23:27:30 +0700

    finished

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,15 +1,40 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchPost, deletePost } from '../actions/index';
 
 class PostsShow extends Component {
+  componentDidMount() {
+    if (!this.props.posts) {
+      const { id } = this.props.match.params;
+      this.props.fetchPost(id);
+    }
+  }
+
+  onDeleteClick() {
+    const { id } = this.props.match.params;
+    this.props.deletePost(id, () => this.props.history.push('/'));
+  }
 
   render() {
+    const { post } = this.props;
+    if (!post) {
+      return <div>Loading...</div>;
+    }
     return (
       <div>
-        Posts Show!
+        <Link to="/" >Back to index</Link>
+        <button className="btn btn-danger pull-xs-right" onClick={this.onDeleteClick.bind(this)}>Delete Post</button>
+        <h3>{post.title}</h3>
+        <h3>Categories: {post.categories}</h3>
+        <p>{post.content}</p>
       </div>
     );
   }
-
 }
 
-export default PostsShow;
+function mapStateToProps({ posts }, ownProps) {
+  return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
